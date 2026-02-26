e95418799ca4e465f1e56514539684306c07eca5
joaopeixotosoares-max <joao.peixoto.soares@escola.pr.gov.br>
2026-02-26 11:57:16 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+let nomealuno = "joão";
+
+const idadealuno = 16;
+
+const estamatriculado = true;
+
+console.log(`aluno ${nomealuno}, tem ${idadealuno} anos, status da matrícula: ${estamatriculado}`);
